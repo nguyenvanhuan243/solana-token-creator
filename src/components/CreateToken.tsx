daed60eefb27b50f695ec498281ed580bad75971
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-12-01 16:21:59 +0700

    Update create token

--- src/components/CreateToken.tsx
+++ src/components/CreateToken.tsx
@@ -15,78 +15,78 @@ export const CreateToken: FC = () => {
 
   const onClick = useCallback(async (form) => {
     try {
-        const lamports = await getMinimumBalanceForRentExemptMint(connection);
-        const mintKeypair = Keypair.generate();
-        const tokenATA = await getAssociatedTokenAddress(mintKeypair.publicKey, publicKey);
+      const lamports = await getMinimumBalanceForRentExemptMint(connection);
+      const mintKeypair = Keypair.generate();
+      const tokenATA = await getAssociatedTokenAddress(mintKeypair.publicKey, publicKey);
 
-        const createMetadataInstruction = createCreateMetadataAccountV3Instruction(
-          {
-            metadata: PublicKey.findProgramAddressSync(
-              [
-                Buffer.from("metadata"),
-                PROGRAM_ID.toBuffer(),
-                mintKeypair.publicKey.toBuffer(),
-              ],
-              PROGRAM_ID,
-            )[0],
-            mint: mintKeypair.publicKey,
-            mintAuthority: publicKey,
-            payer: publicKey,
-            updateAuthority: publicKey,
-          },
-          {
-            createMetadataAccountArgsV3: {
-              data: {
-                name: form.tokenName,
-                symbol: form.symbol,
-                uri: form.metadata,
-                creators: null,
-                sellerFeeBasisPoints: 0,
-                uses: null,
-                collection: null,
-              },
-              isMutable: false,
-              collectionDetails: null,
+      const createMetadataInstruction = createCreateMetadataAccountV3Instruction(
+        {
+          metadata: PublicKey.findProgramAddressSync(
+            [
+              Buffer.from("metadata"),
+              PROGRAM_ID.toBuffer(),
+              mintKeypair.publicKey.toBuffer(),
+            ],
+            PROGRAM_ID,
+          )[0],
+          mint: mintKeypair.publicKey,
+          mintAuthority: publicKey,
+          payer: publicKey,
+          updateAuthority: publicKey,
+        },
+        {
+          createMetadataAccountArgsV3: {
+            data: {
+              name: form.tokenName,
+              symbol: form.symbol,
+              uri: form.metadata,
+              creators: null,
+              sellerFeeBasisPoints: 0,
+              uses: null,
+              collection: null,
             },
+            isMutable: false,
+            collectionDetails: null,
           },
-        );
+        },
+      );
 
-        const createNewTokenTransaction = new Transaction().add(
-          SystemProgram.createAccount({
-              fromPubkey: publicKey,
-              newAccountPubkey: mintKeypair.publicKey,
-              space: MINT_SIZE,
-              lamports: lamports,
-              programId: TOKEN_PROGRAM_ID,
-          }),
-          createInitializeMintInstruction(
-            mintKeypair.publicKey, 
-            form.decimals, 
-            publicKey, 
-            publicKey, 
-            TOKEN_PROGRAM_ID),
-          createAssociatedTokenAccountInstruction(
-            publicKey,
-            tokenATA,
-            publicKey,
-            mintKeypair.publicKey,
-          ),
-          createMintToInstruction(
-            mintKeypair.publicKey,
-            tokenATA,
-            publicKey,
-            form.amount * Math.pow(10, form.decimals),
-          ),
-          createMetadataInstruction
-        );
-        await sendTransaction(createNewTokenTransaction, connection, {signers: [mintKeypair]});
-        
-        // Success feedback
-        alert("Token created successfully!");
+      const createNewTokenTransaction = new Transaction().add(
+        SystemProgram.createAccount({
+          fromPubkey: publicKey,
+          newAccountPubkey: mintKeypair.publicKey,
+          space: MINT_SIZE,
+          lamports: lamports,
+          programId: TOKEN_PROGRAM_ID,
+        }),
+        createInitializeMintInstruction(
+          mintKeypair.publicKey,
+          form.decimals,
+          publicKey,
+          publicKey,
+          TOKEN_PROGRAM_ID),
+        createAssociatedTokenAccountInstruction(
+          publicKey,
+          tokenATA,
+          publicKey,
+          mintKeypair.publicKey,
+        ),
+        createMintToInstruction(
+          mintKeypair.publicKey,
+          tokenATA,
+          publicKey,
+          form.amount * Math.pow(10, form.decimals),
+        ),
+        createMetadataInstruction
+      );
+      await sendTransaction(createNewTokenTransaction, connection, { signers: [mintKeypair] });
+
+      // Success feedback
+      alert("Token created successfully!");
     } catch (error) {
-        // Error handling
-        console.error("Token creation failed:", error);
-        alert("Token creation failed. Please try again.");
+      // Error handling
+      console.error("Token creation failed:", error);
+      alert("Token creation failed. Please try again.");
     }
   }, [publicKey, connection, sendTransaction]);
 
@@ -95,19 +95,19 @@ export const CreateToken: FC = () => {
       <input
         type="text"
         className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-        placeholder="Token Name"
+        placeholder="Name"
         onChange={(e) => setTokenName(e.target.value)}
       />
       <input
         type="text"
         className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-        placeholder="Symbol"
+        placeholder="Ticker"
         onChange={(e) => setSymbol(e.target.value)}
       />
       <input
         type="text"
         className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-        placeholder="Metadata Url"
+        placeholder="Image or video"
         onChange={(e) => setMetadata(e.target.value)}
       />
       <input
@@ -122,11 +122,17 @@ export const CreateToken: FC = () => {
         placeholder="Decimals"
         onChange={(e) => setDecimals(e.target.value)}
       />
-      
+
       <button
         className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
-        onClick={() => onClick({decimals: Number(decimals), amount: Number(amount), metadata: metadata, symbol: symbol, tokenName: tokenName})}>
-          <span>Create Token</span>
+        onClick={() => onClick({
+          decimals: Number(decimals),
+          amount: Number(amount),
+          metadata: metadata,
+          symbol: symbol,
+          tokenName: tokenName
+        })}>
+        <span>Create Token</span>
       </button>
     </div>
   )
