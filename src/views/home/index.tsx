dbce51237d53aa82b6725dcb8f9b8a572a7ab1e1
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-12-01 16:09:51 +0700

    Update home create token

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -29,13 +29,21 @@ export const HomeView: FC = ({ }) => {
     }
   }, [wallet.publicKey, connection, getUserSOLBalance])
 
+  const renderCreateTokenHeader = () => {
+    return (
+      <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+        Solana Token Creator
+      </h1>
+    )
+  }
+
   return (
 
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
-        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-          Token Creator
-        </h1>      
+        {
+          renderCreateTokenHeader()
+        }
         <div className="text-center">
           {/* <RequestAirdrop  my-2/> */}
           {wallet && <p>SOL Balance: {(balance || 0).toLocaleString()}</p>}
