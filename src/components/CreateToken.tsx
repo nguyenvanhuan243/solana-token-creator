19109e980e801ac43fd027d8d76a99577acf35e4
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-12-01 21:30:04 +0700

    update fifle

--- src/components/CreateToken.tsx
+++ src/components/CreateToken.tsx
@@ -111,7 +111,7 @@ export const CreateToken: FC = () => {
         onChange={(e) => setSymbol(e.target.value)}
       />
       <input
-        type="text"
+        type="file"
         className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
         placeholder="Image/video (Backend)"
         onChange={(e) => setMetadata(e.target.value)}
