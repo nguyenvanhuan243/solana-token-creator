ad77888e7d2ec7b0ea7a40e2a78782f3fad0ce9f
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-12-03 09:13:47 +0700

    buy sell tokne

---
+++ src/components/BuySellToken.tsx
@@ -0,0 +1,141 @@
+import { FC, useCallback, useState } from 'react';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { Keypair, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import {
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+  createInitializeMintInstruction,
+  getMinimumBalanceForRentExemptMint,
+  getAssociatedTokenAddress,
+  createAssociatedTokenAccountInstruction,
+  createMintToInstruction
+} from '@solana/spl-token';
+import { createCreateMetadataAccountV3Instruction, PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata';
+
+export const BuySellToken: FC = () => {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+  const [tokenName, setTokenName] = useState('')
+  const [symbol, setSymbol] = useState('')
+
+  // Supply 21_000_000 Token
+  const AMOUNT_TOKEN_SUPPLY = BigInt("21000000000000000");
+  const SOLANA_DECIMALS = 9;
+
+  const onClick = useCallback(async (form) => {
+    try {
+      const lamports = await getMinimumBalanceForRentExemptMint(connection);
+      const mintKeypair = Keypair.generate();
+      const tokenATA = await getAssociatedTokenAddress(mintKeypair.publicKey, publicKey);
+
+      const createMetadataInstruction = createCreateMetadataAccountV3Instruction(
+        {
+          metadata: PublicKey.findProgramAddressSync(
+            [
+              Buffer.from("metadata"),
+              PROGRAM_ID.toBuffer(),
+              mintKeypair.publicKey.toBuffer(),
+            ],
+            PROGRAM_ID,
+          )[0],
+          mint: mintKeypair.publicKey,
+          mintAuthority: publicKey,
+          payer: publicKey,
+          updateAuthority: publicKey,
+        },
+        {
+          createMetadataAccountArgsV3: {
+            data: {
+              name: form.tokenName,
+              symbol: form.symbol,
+              uri: "",
+              creators: null,
+              sellerFeeBasisPoints: 0,
+              uses: null,
+              collection: null,
+            },
+            isMutable: false,
+            collectionDetails: null,
+          },
+        },
+      );
+
+      const createNewTokenTransaction = new Transaction().add(
+        SystemProgram.createAccount({
+          fromPubkey: publicKey,
+          newAccountPubkey: mintKeypair.publicKey,
+          space: MINT_SIZE,
+          lamports: lamports,
+          programId: TOKEN_PROGRAM_ID,
+        }),
+        createInitializeMintInstruction(
+          mintKeypair.publicKey,
+          form.decimals,
+          publicKey,
+          publicKey,
+          TOKEN_PROGRAM_ID),
+        createAssociatedTokenAccountInstruction(
+          publicKey,
+          tokenATA,
+          publicKey,
+          mintKeypair.publicKey,
+        ),
+        createMintToInstruction(
+          mintKeypair.publicKey,
+          tokenATA,
+          publicKey,
+          form.amount,
+        ),
+        createMetadataInstruction
+      );
+      await sendTransaction(createNewTokenTransaction, connection, { signers: [mintKeypair] });
+
+      // Success feedback
+      alert("Token created successfully!");
+    } catch (error) {
+      // Error handling
+      console.error("Token creation failed:", error);
+      alert("Token creation failed. Please try again.");
+    }
+  }, [publicKey, connection, sendTransaction]);
+
+  return (
+    <div className="my-6">
+      <input
+        disabled
+        value="0x570a5d26f7765ecb712c0924e4de545b89fd43df"
+        type="text"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Token contract"
+        onChange={(e) => setTokenName(e.target.value)}
+      />
+      <input
+        type="text"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Enter your amount"
+        onChange={(e) => setSymbol(e.target.value)}
+      />
+
+      <button
+        className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
+        onClick={() => onClick({
+          decimals: Number(SOLANA_DECIMALS),
+          amount: AMOUNT_TOKEN_SUPPLY,
+          symbol: symbol,
+          tokenName: tokenName
+        })}>
+        <span>BUY Token</span>
+      </button>
+      <button
+        className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
+        onClick={() => onClick({
+          decimals: Number(SOLANA_DECIMALS),
+          amount: AMOUNT_TOKEN_SUPPLY,
+          symbol: symbol,
+          tokenName: tokenName
+        })}>
+        <span>SELL TOKEN</span>
+      </button>
+    </div>
+  )
+}
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,3 +3,5 @@ export { MetadataView } from './metadata';
 export { UploaderView } from './uploader';
 export { BasicsView } from "./basics";
 export { UpdateView } from "./update";
+export { TradingView } from "./trading";
+
