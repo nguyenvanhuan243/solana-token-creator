82b1bf15ce75b54332c060452568b614ab4892db
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-12-06 10:20:41 +0700

    add rel=noreferrer

--- src/components/CreateToken.tsx
+++ src/components/CreateToken.tsx
@@ -140,6 +140,7 @@ export const CreateToken: FC = () => {
 
 
       <a
+        rel="noreferrer"
         className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
         href="https://t.me/huancapital"
         target="_blank">
